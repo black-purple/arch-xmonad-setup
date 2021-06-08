d3e4e4d45dad806934697b95c11b360d8eb9e55b
black-purple <dakirallah.ar@gmail.com>
2021-06-08 15:41:17 +0100

    added soe aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,19 +1,20 @@
-#/bin/bash
+#!/bin/sh
 
-
-alias clock='tty-clock -scC 3'
+# calling scripts
 alias dt=~/BashScripts/mygreetings.sh
 alias ks=~/BashScripts/lay_sw.sh
+alias kp=~/BashScripts/kp.sh
 
 # one letter commands
 alias c='clear && dt'
 alias e=exit
 alias r=ranger
 alias v='devour mpv'
+alias d=devour
 
 # config commands
-alias xc='nvim ~/.xmonad/xmonad.hs'
-alias ac='nvim ~/.config/alacritty/alacritty.yml'
+alias xc='nvim ~/.xmonad/xmonad.hs; cp ~/.xmonad/xmonad.hs ~/dotfiles/configs/'
+alias ac='nvim ~/.config/alacritty/alacritty.yml; cp ~/.config/alacritty/alacritty.yml ~/dotfiles/configs/'
 alias brc='nvim ~/.bashrc'
 alias frc='nvim ~/.config/fish/config.fish'
 
@@ -24,8 +25,8 @@ alias psr='pacman -Ss'
 alias up='sudo pacman -Syu'
 
 # aur commands
-alias ain='paru -S'
-alias aup='paru -Syu'
+alias ain='yay -S'
+alias aup='yay -Syu'
 
 # replacement commands
 alias doas='doas --'
@@ -33,4 +34,15 @@ alias cat=bat
 alias ls=lsd
 alias rm='rm -i'
 alias nv=nvim
+alias clock='tty-clock -scC 3'
 
+# ls shortcuts
+alias la='lsd -lhatr'
+
+# git shortcuts
+alias gts='git status'
+alias gta='git add'
+alias gtaa='git add .'
+alias gtb='git branch'
+alias gtc='git commit -m'
+alias gtp='git push -u origin'
